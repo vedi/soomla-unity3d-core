9c8ce2a6288d9e3369d3902b005ec356a65d4271
Eugene Butusov <EvgenyButusov@gmail.com>
2015-10-18 17:14:27 +0300

    [IMPROVE] Put all SOOMLA stuff in one folder in Unity
    process borisspektor's feedback

--- Soomla/Assets/Soomla/Editor/build-tools/Soomla_CoreRunner.py
+++ Soomla/Assets/Soomla/Editor/build-tools/Soomla_CoreRunner.py
@@ -48,7 +48,7 @@ for framework in frameworks:
   pbx_object.add_file_if_doesnt_exist(framework, tree='SDKROOT')
 
 for framework in weak_frameworks:
-  pbx_object.add_file_if_doesnt_exist(framework, tree='SDKROOT', weak=True)\
+  pbx_object.add_file_if_doesnt_exist(framework, tree='SDKROOT', weak=True)
 
 pbx_object.add_other_ldflags('-ObjC')
 
