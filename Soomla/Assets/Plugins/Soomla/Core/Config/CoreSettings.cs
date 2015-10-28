5f0787f1408c7bc84d59c15aaddf0558c7f415b0
Eugene Butusov <EvgenyButusov@gmail.com>
2015-10-28 09:53:16 +0300

    Fix version number

--- Soomla/Assets/Plugins/Soomla/Core/Config/CoreSettings.cs
+++ Soomla/Assets/Plugins/Soomla/Core/Config/CoreSettings.cs
@@ -37,7 +37,7 @@ namespace Soomla
 
 		static CoreSettings instance = new CoreSettings();
 
-		static string currentModuleVersion = "1.0.10";
+		static string currentModuleVersion = "1.1.0";
 
 		static CoreSettings()
 		{
