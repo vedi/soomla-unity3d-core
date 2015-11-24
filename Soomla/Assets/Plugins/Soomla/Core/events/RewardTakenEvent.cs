a9cdf4e278d044051fd8addff30e8036bc92be60
Pavel <Blink.quet@gmail.com>
2015-11-24 18:45:57 +0300

    Code Refactoring

--- Soomla/Assets/Plugins/Soomla/Core/events/RewardTakenEvent.cs
+++ Soomla/Assets/Plugins/Soomla/Core/events/RewardTakenEvent.cs
@@ -5,7 +5,7 @@ namespace Soomla
 {
 	public class RewardTakenEvent : SoomlaEvent
 	{
-		public readonly Reward mReward;
+		public readonly Reward Reward;
 
 		public RewardTakenEvent (String rewardId) : this(rewardId, null)
 		{
@@ -19,12 +19,12 @@ namespace Soomla
 
 		public RewardTakenEvent (String rewardId, Object sender) : base(sender)
 		{
-			mReward = Reward.GetReward (rewardId);
+			this.Reward = Reward.GetReward (rewardId);
 		}
 
 		public RewardTakenEvent (Reward reward, Object sender) : base(sender)
 		{
-			mReward = reward;
+			this.Reward = reward;
 		}
 	}
 }
