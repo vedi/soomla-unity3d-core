2e44cfabed606c6c2e703fccfb3574e7461f884e
Eugene Butusov <EvgenyButusov@gmail.com>
2015-11-24 19:56:26 +0300

    Align spaces

--- Soomla/Assets/Plugins/Soomla/Core/events/SoomlaEvent.cs
+++ Soomla/Assets/Plugins/Soomla/Core/events/SoomlaEvent.cs
@@ -6,20 +6,20 @@ namespace Soomla
 {
 	public class SoomlaEvent
 	{
-        public readonly Object Sender;
+		public readonly Object Sender;
 		public readonly String Payload;
 
-        public SoomlaEvent() { }
+		public SoomlaEvent() { }
 
-        public SoomlaEvent(Object sender) : this(sender, "")
-        { 	
-			
+		public SoomlaEvent(Object sender) : this(sender, "")
+		{
+
 		}
 
 		public SoomlaEvent(Object sender, String payload)
-        {
+		{
 			this.Sender = sender;
 			this.Payload = payload;
-        }
+		}
     }
 }
